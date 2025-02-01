5d6542d9f0ab6dd101d2ceed4c370376eeb9b745
q1b <sukhpreetben10@gmail.com>
2025-02-01 07:22:34 +0530

    Defined: userRelations and sessionRelations

--- src/lib/server/db/schema/session.ts
+++ src/lib/server/db/schema/session.ts
@@ -1,6 +1,7 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 import { userTable } from './user';
 import { id } from '../utils';
+import { relations } from 'drizzle-orm';
 
 export const sessionTable = sqliteTable('session', {
 	id,
@@ -9,5 +10,12 @@ export const sessionTable = sqliteTable('session', {
 		.references(() => userTable.id, { onDelete: 'cascade' }),
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
+
+export const sessionRelations = relations(sessionTable, ({ one }) => ({
+	user: one(userTable, {
+		fields: [sessionTable.userId],
+		references: [userTable.id]
+	})
+}))
 
 export type Session = typeof sessionTable.$inferSelect;
--- src/lib/server/db/schema/user.ts
+++ src/lib/server/db/schema/user.ts
@@ -1,5 +1,7 @@
 import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { createdAt, id } from '../utils';
+import { relations } from 'drizzle-orm';
+import { sessionTable } from './session';
 
 export const userTable = sqliteTable('user', {
 	id,
@@ -7,5 +9,9 @@ export const userTable = sqliteTable('user', {
 	name: text('name'),
 	createdAt
 });
+
+export const userRelations = relations(userTable, ({ many }) => ({
+	sessions: many(sessionTable),
+}))
 
 export type User = typeof userTable.$inferSelect;
