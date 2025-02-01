f496531c0fae856dc780793fe0ff263a74e7d22e
q1b <sukhpreetben10@gmail.com>
2025-02-01 10:47:44 +0530

    FIX: Build Issue, updated import-path

--- src/routes/app/settings/+page.svelte
+++ src/routes/app/settings/+page.svelte
@@ -1,31 +1,35 @@
 <script lang="ts">
-	import NotificationButton from "./notification.svelte"
+	import NotificationButton from './notification-button.svelte';
 	import { onMount } from 'svelte';
 
 	let installPrompt = $state(null);
 	let installButton = $state(null);
 	function disableInAppInstallPrompt() {
-  		installPrompt = null;
-  		installButton.setAttribute("hidden", "");
+		installPrompt = null;
+		installButton.setAttribute('hidden', '');
 	}
 
 	onMount(() => {
-		window.addEventListener("beforeinstallprompt", (event) => {
+		window.addEventListener('beforeinstallprompt', (event) => {
 			event.preventDefault();
-  			installPrompt = event;
-  			installButton.removeAttribute("hidden");
+			installPrompt = event;
+			installButton.removeAttribute('hidden');
 		});
-		window.addEventListener("appinstalled", () => {
-			disableInAppInstallPrompt()
+		window.addEventListener('appinstalled', () => {
+			disableInAppInstallPrompt();
 		});
-	})
+	});
 </script>
 
-<button bind:this={installButton} onclick={() => {
- 	if (!installPrompt) return;
-  	const result = await installPrompt.prompt();
-   	console.log(`Install prompt was: ${result.outcome}`);
-    disableInAppInstallPrompt()
-}} hidden>Install</button>
+<button
+	bind:this={installButton}
+	onclick={async () => {
+		if (!installPrompt) return;
+		const result = await installPrompt.prompt();
+		console.log(`Install prompt was: ${result.outcome}`);
+		disableInAppInstallPrompt();
+	}}
+	hidden>Install</button
+>
 
 <NotificationButton />
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -16,12 +16,60 @@ precacheAndRoute(self.__WB_MANIFEST);
 // clean old assets
 cleanupOutdatedCaches();
 
-let allowlist: RegExp[] | undefined;
-// in dev mode, we disable precaching to avoid caching issues
-if (import.meta.env.DEV) allowlist = [/^\/$/];
+// let allowlist: RegExp[] | undefined
+// // in dev mode, we disable precaching to avoid caching issues
+// if (import.meta.env.DEV)
+// 	allowlist = [/^\/$/]
 
 // to allow work offline
-registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist }));
+// registerRoute(new NavigationRoute(
+// 	createHandlerBoundToURL('/'),
+// 	{ allowlist },
+// ))
+
+self.addEventListener('push', (event) => {
+	if (!event.data) return;
+
+	const data = event.data.json();
+
+	event.waitUntil(
+		self.registration.showNotification(data.title, {
+			body: data.body,
+			icon: '/pwa-192x192.png', // Add your notification icon
+			badge: '/pwa-192x192.png',
+			data: { url: self.location.origin } // URL to open when notification is clicked
+		})
+	);
+});
+
+self.addEventListener('notificationclick', onNotificationClick);
+
+export function onNotificationClick(event: NotificationEvent) {
+	const reactToNotificationClick = new Promise((resolve) => {
+		event.notification.close();
+		resolve(openUrl(event.notification.data.url));
+	});
+
+	event.waitUntil(reactToNotificationClick);
+}
+
+function findBestClient(clients: WindowClient[]) {
+	const focusedClient = clients.find((client) => client.focused);
+	const visibleClient = clients.find((client) => client.visibilityState === 'visible');
+
+	return focusedClient || visibleClient || clients[0];
+}
+
+async function openUrl(url: string) {
+	const clients = await self.clients.matchAll({ type: 'window' });
+	// Chrome 42-48 does not support navigate
+	if (clients.length !== 0 && 'navigate' in clients[0]) {
+		const client = findBestClient(clients as WindowClient[]);
+		await client.navigate(url).then((client) => client?.focus());
+	}
+
+	await self.clients.openWindow(url);
+}
 
 self.skipWaiting();
 clientsClaim();
